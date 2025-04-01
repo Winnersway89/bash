947f9fd2cf4ae71724a83db6f42cf431339cb859
Winnersway89 <106033571+Winnersway89@users.noreply.github.com>
2025-04-01 20:00:55 +0200

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,1 +1,15 @@
+javascript
 
+const hre = require("hardhat");
+
+async function main() {
+    const MyNFT = await hre.ethers.getContractFactory("MyNFT");
+    const myNFT = await MyNFT.deploy();
+    await myNFT.deployed();
+    console.log(`Contract deployed to: ${myNFT.address}`);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
