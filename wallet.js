404ac7ebbaba42ffcbe1dafe1ae938d6d36dc2ec
Winnersway89 <106033571+Winnersway89@users.noreply.github.com>
2025-04-01 20:04:07 +0200

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,1 +1,21 @@
+javascript
 
+import { ethers } from "ethers";
+
+export async function connectWallet() {
+    if (!window.ethereum) {
+        alert("Please install MetaMask!");
+        return;
+    }
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const address = await signer.getAddress();
+
+    localStorage.setItem("walletAddress", address);
+    return address;
+}
+
+export function disconnectWallet() {
+    localStorage.removeItem("walletAddress");
+}
