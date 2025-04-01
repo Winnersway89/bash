b5d2a3f99cc8b0de0fbf4c43f916a370d4ed1f0a
Winnersway89 <106033571+Winnersway89@users.noreply.github.com>
2025-04-01 19:59:46 +0200

    Update MyNFT.sol

--- MyNFT.sol
+++ MyNFT.sol
@@ -1,1 +1,25 @@
-MyNFT.sol
+solidity
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721Enumerable, Ownable {
+    uint256 public nextTokenId;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mintNFT(address to, string memory tokenURI) public onlyOwner {
+        uint256 tokenId = nextTokenId;
+        _mint(to, tokenId);
+        _tokenURIs[tokenId] = tokenURI;
+        nextTokenId++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
+}
